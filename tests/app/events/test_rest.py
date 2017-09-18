c360523f1a2ba5be5cb20aca830eac11559da970
Ken Tsang <kenlt.uk@gmail.com>
2017-09-19 00:36:08 +0100

    Refactored events test

--- tests/app/events/test_rest.py
+++ tests/app/events/test_rest.py
@@ -8,10 +8,8 @@ def events_page(client):
     return request(url_for('events.get_events'), client.get)
 
 
-class WhenCallingEventEndpoint(object):
+class WhenGettingEvents(object):
 
-    def it_returns_all_events(self, sample_events, events_page, db_session):
-        events = events_page
-        print(events.get_data(as_text=True))
-        data = json.loads(events.get_data(as_text=True))['data']
-        assert len(data) == len(sample_events)
+    def it_returns_all_events(self, sample_event, events_page, db_session):
+        data = json.loads(events_page.get_data(as_text=True))['data']
+        assert len(data) == 1
