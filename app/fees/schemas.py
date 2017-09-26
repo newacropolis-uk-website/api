ac336e5d8549a130cb86fa9b295a0bb39272cbb7
Ken Tsang <kenlt.uk@gmail.com>
2017-09-26 23:36:35 +0100

    Update fees rest

--- app/fees/schemas.py
+++ app/fees/schemas.py
@@ -11,3 +11,17 @@ post_create_fee_schema = {
     },
     "required": ["fee", "conc_fee"]
 }
+
+
+post_update_fee_schema = {
+    "$schema": "http://json-schema.org/draft-04/schema#",
+    "description": "POST schema for updating fee",
+    "type": "object",
+    "properties": {
+        "fee": {"type": "integer"},
+        "conc_fee": {"type": "integer"},
+        "multi_day_fee": {"type": ["integer", "null"]},
+        "multi_day_conc_fee": {"type": ["integer", "null"]},
+        "valid_from": {"type": ["string", "null"], "format": "datetime"}
+    },
+}
