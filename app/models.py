956c9c862c295f57fdd8b4955a2d149f0f619df3
kenlt-uk <kenlt.uk@gmail.com>
2017-09-30 00:49:57 +0100

    Update data model

--- app/models.py
+++ app/models.py
@@ -12,12 +12,23 @@ class EventType(db.Model):
     __tablename__ = 'event_types'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     old_id = db.Column(db.Integer)
-    event_type = db.Column(db.String(255), unique=True)
+    event_type = db.Column(db.String(255), unique=True, nullable=False)
     event_desc = db.Column(db.String())
     event_filename = db.Column(db.String(255))
     duration = db.Column(db.Integer)
     repeat = db.Column(db.Integer)
     repeat_interval = db.Column(db.Integer)
+
+    def serialize(self):
+        return {
+            'id': str(self.id),
+            'event_type': self.event_type,
+            'event_desc': self.event_desc,
+            'event_filename': self.event_filename,
+            'duration': self.duration,
+            'repeat': self.repeat,
+            'repeat_interval': self.repeat_interval
+        }
 
 
 class Fee(db.Model):
