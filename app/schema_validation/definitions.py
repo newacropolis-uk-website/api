de1a846c3d03192c96057d206d9fdefcf3853031
kenlt-uk <kenlt.uk@gmail.com>
2018-04-23 23:45:11 +0100

    Add datetime to schema validation

--- app/schema_validation/definitions.py
+++ app/schema_validation/definitions.py
@@ -3,3 +3,9 @@ uuid = {
     "pattern": "^[a-fA-F0-9]{8}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{4}-[a-fA-F0-9]{12}$",
     "validationMessage": "is not a valid UUID",
 }
+
+datetime = {
+    "type": "string",
+    "pattern": "^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}$",
+    "validationMessage": "is not a datetime in format YYYY-MM-DD HH:MM",
+}
