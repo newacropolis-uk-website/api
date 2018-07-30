a4fb5729453f0f3a4221ef615a4d507a9321ae81
kenlt-uk <kenlt.uk@gmail.com>
2018-07-30 14:08:23 +0100

    Add example environment

---
+++ environment_sample.sh
@@ -0,0 +1,16 @@
+# environment
+export ENVIRONMENT=
+export DATABASE_URL_development=
+export API_development=
+
+# authentication
+export ADMIN_CLIENT_ID=
+export ADMIN_CLIENT_SECRET=
+
+# testing
+export API_BASE_URL=
+
+# deployment
+export user=
+export deploy_host=
+export debug=
