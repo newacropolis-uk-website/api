7b862e5ec071320c61f616a59fcaaa7bacbe59ea
kenlt-uk <kenlt.uk@gmail.com>
2018-08-04 23:47:34 +0100

    Update env vars to deploy and gitignore

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -34,6 +34,7 @@ if [ $port != 'No environment' ]; then
     export DATABASE_URL_$environment=$DATABASE_URL_ENV
     export ADMIN_CLIENT_ID=$ADMIN_CLIENT_ID
     export ADMIN_CLIENT_SECRET=$ADMIN_CLIENT_SECRET
+    export JWT_SECRET=$JWT_SECRET
     sudo ./scripts/bootstrap.sh
     ./scripts/run_app.sh $environment gunicorn $output_params"""
 
