55c1ab4faa872d7359f48f97a2a0b2f2187f4de4
kenlt-uk <kenlt.uk@gmail.com>
2018-12-19 00:45:26 +0000

    Send stacktrace to logs for DecodeError

--- app/errors.py
+++ app/errors.py
@@ -69,7 +69,7 @@ def register_errors(blueprint):
     @blueprint.errorhandler(DecodeError)
     def decode_error(e):
         msg = 'Decode error on auth token'
-        current_app.logger.exception(msg, e.message)
+        current_app.logger.exception(e)
         return jsonify(result='error', message=str(msg)), 400
 
     @blueprint.errorhandler(InvalidHeaderError)
