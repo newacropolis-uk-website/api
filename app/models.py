f355944bbbde4cb2c7d09497cdd4f3b929d32db3
kenlt-uk <kenlt.uk@gmail.com>
2019-06-15 10:27:32 +0100

    Re order db models

--- app/models.py
+++ app/models.py
@@ -9,86 +9,44 @@ from sqlalchemy.ext.hybrid import hybrid_property
 from app import db
 
 
-class TokenBlacklist(db.Model):
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    jti = db.Column(db.String(36), nullable=False)
-    token_type = db.Column(db.String(10), nullable=False)
-    user_identity = db.Column(db.String(50), nullable=False)
-    revoked = db.Column(db.Boolean, nullable=False)
-    expires = db.Column(db.DateTime, nullable=False)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+class Article(db.Model):
+    __tablename__ = 'articles'
 
-    def serialize(self):
-        return {
-            'token_id': self.id,
-            'jti': self.jti,
-            'token_type': self.token_type,
-            'user_identity': self.user_identity,
-            'revoked': self.revoked,
-            'expires': self.expires
-        }
-
-
-class EventType(db.Model):
-    __tablename__ = 'event_types'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     old_id = db.Column(db.Integer)
-    event_type = db.Column(db.String(255), unique=True, nullable=False)
-    event_desc = db.Column(db.String())
-    event_filename = db.Column(db.String(255))
-    duration = db.Column(db.Integer)
-    repeat = db.Column(db.Integer)
-    repeat_interval = db.Column(db.Integer)
+    title = db.Column(db.String(255))
+    author = db.Column(db.String(255))
+    content = db.Column(db.Text())
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def serialize(self):
-        def fees():
-            _fees = []
-            for fee in self.fees:
-                _fees.append({
-                    'fee': fee.fee,
-                    'conc_fee': fee.conc_fee,
-                    'valid_from': fee.valid_from.isoformat()
-                })
-
-            _fees = sorted(_fees, key=lambda f: f['valid_from'], reverse=True)
-            return _fees
-
         return {
             'id': str(self.id),
             'old_id': self.old_id,
-            'event_type': self.event_type,
-            'event_desc': self.event_desc,
-            'event_filename': self.event_filename,
-            'duration': self.duration,
-            'repeat': self.repeat,
-            'repeat_interval': self.repeat_interval,
-            'fees': fees(),
-            'created_at': self.created_at
+            'title': self.title,
+            'author': self.author,
+            'content': self.content,
+            'created_at': self.created_at.strftime('%Y-%m-%d') if self.created_at else None,
         }
 
+    def serialize_summary(self):
+        def get_short_content(num_words):
+            html_tag_pattern = r'<.*?>'
+            clean_content = re.sub(html_tag_pattern, '', self.content)
 
-class Fee(db.Model):
-    __tablename__ = 'fees'
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    fee = db.Column(db.Integer, nullable=False)
-    conc_fee = db.Column(db.Integer, nullable=False)
-    multi_day_fee = db.Column(db.Integer, nullable=True)
-    multi_day_conc_fee = db.Column(db.Integer, nullable=True)
-    valid_from = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
-    event_type_id = db.Column(UUID(as_uuid=True), db.ForeignKey('event_types.id'), nullable=False)
-    event_type = db.relationship(EventType, backref=db.backref("fees", order_by=valid_from.desc()))
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+            content_arr = clean_content.split(' ')
+            if len(content_arr) > num_words:
+                find_words = " ".join([content_arr[num_words - 2], content_arr[num_words - 1], content_arr[num_words]])
+                return clean_content[0:clean_content.index(find_words) + len(find_words)] + '...'
+            else:
+                return clean_content
 
-    def serialize(self):
         return {
             'id': str(self.id),
-            'event_type_id': str(self.event_type_id),
-            'fee': self.fee,
-            'conc_fee': self.conc_fee,
-            'multi_day_fee': self.multi_day_fee,
-            'multi_day_conc_fee': self.multi_day_conc_fee,
-            'valid_from': self.valid_from.isoformat()
+            'title': self.title,
+            'author': self.author,
+            'short_content': get_short_content(num_words=110),
+            'very_short_content': get_short_content(num_words=30),
         }
 
 
@@ -100,12 +58,6 @@ REJECTED = 'rejected'
 EVENT_STATES = [
     DRAFT, READY, APPROVED, REJECTED
 ]
-
-
-class EventStates(db.Model):
-    __tablename__ = 'event_states'
-
-    name = db.Column(db.String(), primary_key=True)
 
 
 class Event(db.Model):
@@ -193,49 +145,6 @@ class Event(db.Model):
         return '<Event: id {}>'.format(self.id)
 
 
-class RejectReason(db.Model):
-    __tablename__ = 'reject_reasons'
-
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    event_id = db.Column(UUID(as_uuid=True), db.ForeignKey('events.id'))
-    reason = db.Column(db.String(255), nullable=False)
-    resolved = db.Column(db.Boolean, default=False)
-    created_by = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-
-    def serialize(self):
-        return {
-            'id': str(self.id),
-            'reason': self.reason,
-            'resolved': self.resolved,
-            'created_by': self.created_by,
-            'created_at': self.created_at
-        }
-
-
-class Speaker(db.Model):
-    __tablename__ = 'speakers'
-
-    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    title = db.Column(db.String(100))
-    name = db.Column(db.String(255))
-    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
-    # should only expect 1 parent at most, so a parent cannot be a parent
-    parent_id = db.Column(UUID(as_uuid=True), primary_key=False, default=None, nullable=True)
-
-    def serialize(self):
-        return {
-            'id': str(self.id),
-            'title': self.title,
-            'name': self.name,
-            'parent_id': str(self.parent_id) if self.parent_id else None
-        }
-
-    @hybrid_property
-    def last_name(self):
-        return str(self.name).split(' ')[-1]
-
-
 event_date_to_speaker = db.Table(
     'event_date_to_speaker',
     db.Model.metadata,
@@ -280,66 +189,135 @@ class EventDate(db.Model):
         }
 
 
-class Venue(db.Model):
-    __tablename__ = 'venues'
+class EventStates(db.Model):
+    __tablename__ = 'event_states'
 
+    name = db.Column(db.String(), primary_key=True)
+
+
+class EventType(db.Model):
+    __tablename__ = 'event_types'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     old_id = db.Column(db.Integer)
-    name = db.Column(db.String(255))
-    address = db.Column(db.String(255))
-    directions = db.Column(db.String(255))
-
-    default = db.Column(db.Boolean)
+    event_type = db.Column(db.String(255), unique=True, nullable=False)
+    event_desc = db.Column(db.String())
+    event_filename = db.Column(db.String(255))
+    duration = db.Column(db.Integer)
+    repeat = db.Column(db.Integer)
+    repeat_interval = db.Column(db.Integer)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def serialize(self):
+        def fees():
+            _fees = []
+            for fee in self.fees:
+                _fees.append({
+                    'fee': fee.fee,
+                    'conc_fee': fee.conc_fee,
+                    'valid_from': fee.valid_from.isoformat()
+                })
+
+            _fees = sorted(_fees, key=lambda f: f['valid_from'], reverse=True)
+            return _fees
+
         return {
             'id': str(self.id),
             'old_id': self.old_id,
-            'name': str(self.name),
-            'address': self.address,
-            'directions': self.directions,
-            'default': self.default,
+            'event_type': self.event_type,
+            'event_desc': self.event_desc,
+            'event_filename': self.event_filename,
+            'duration': self.duration,
+            'repeat': self.repeat,
+            'repeat_interval': self.repeat_interval,
+            'fees': fees(),
+            'created_at': self.created_at
         }
 
 
-class Article(db.Model):
-    __tablename__ = 'articles'
-
+class Fee(db.Model):
+    __tablename__ = 'fees'
     id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-    old_id = db.Column(db.Integer)
-    title = db.Column(db.String(255))
-    author = db.Column(db.String(255))
-    content = db.Column(db.Text())
+    fee = db.Column(db.Integer, nullable=False)
+    conc_fee = db.Column(db.Integer, nullable=False)
+    multi_day_fee = db.Column(db.Integer, nullable=True)
+    multi_day_conc_fee = db.Column(db.Integer, nullable=True)
+    valid_from = db.Column(db.DateTime, nullable=False, default=datetime.datetime.utcnow)
+    event_type_id = db.Column(UUID(as_uuid=True), db.ForeignKey('event_types.id'), nullable=False)
+    event_type = db.relationship(EventType, backref=db.backref("fees", order_by=valid_from.desc()))
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
 
     def serialize(self):
         return {
             'id': str(self.id),
-            'old_id': self.old_id,
-            'title': self.title,
-            'author': self.author,
-            'content': self.content,
-            'created_at': self.created_at.strftime('%Y-%m-%d') if self.created_at else None,
+            'event_type_id': str(self.event_type_id),
+            'fee': self.fee,
+            'conc_fee': self.conc_fee,
+            'multi_day_fee': self.multi_day_fee,
+            'multi_day_conc_fee': self.multi_day_conc_fee,
+            'valid_from': self.valid_from.isoformat()
         }
 
-    def serialize_summary(self):
-        def get_short_content(num_words):
-            html_tag_pattern = r'<.*?>'
-            clean_content = re.sub(html_tag_pattern, '', self.content)
 
-            content_arr = clean_content.split(' ')
-            if len(content_arr) > num_words:
-                find_words = " ".join([content_arr[num_words - 2], content_arr[num_words - 1], content_arr[num_words]])
-                return clean_content[0:clean_content.index(find_words) + len(find_words)] + '...'
-            else:
-                return clean_content
+class RejectReason(db.Model):
+    __tablename__ = 'reject_reasons'
 
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    event_id = db.Column(UUID(as_uuid=True), db.ForeignKey('events.id'))
+    reason = db.Column(db.String(255), nullable=False)
+    resolved = db.Column(db.Boolean, default=False)
+    created_by = db.Column(UUID(as_uuid=True), db.ForeignKey('users.id'), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def serialize(self):
+        return {
+            'id': str(self.id),
+            'reason': self.reason,
+            'resolved': self.resolved,
+            'created_by': self.created_by,
+            'created_at': self.created_at
+        }
+
+
+class Speaker(db.Model):
+    __tablename__ = 'speakers'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    title = db.Column(db.String(100))
+    name = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    # should only expect 1 parent at most, so a parent cannot be a parent
+    parent_id = db.Column(UUID(as_uuid=True), primary_key=False, default=None, nullable=True)
+
+    def serialize(self):
         return {
             'id': str(self.id),
             'title': self.title,
-            'author': self.author,
-            'short_content': get_short_content(num_words=110),
-            'very_short_content': get_short_content(num_words=30),
+            'name': self.name,
+            'parent_id': str(self.parent_id) if self.parent_id else None
+        }
+
+    @hybrid_property
+    def last_name(self):
+        return str(self.name).split(' ')[-1]
+
+
+class TokenBlacklist(db.Model):
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    jti = db.Column(db.String(36), nullable=False)
+    token_type = db.Column(db.String(10), nullable=False)
+    user_identity = db.Column(db.String(50), nullable=False)
+    revoked = db.Column(db.Boolean, nullable=False)
+    expires = db.Column(db.DateTime, nullable=False)
+    created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+
+    def serialize(self):
+        return {
+            'token_id': self.id,
+            'jti': self.jti,
+            'token_type': self.token_type,
+            'user_identity': self.user_identity,
+            'revoked': self.revoked,
+            'expires': self.expires
         }
 
 
@@ -374,3 +352,25 @@ class User(db.Model):
 
     def is_admin(self):
         return self.access_area == USER_ADMIN
+
+
+class Venue(db.Model):
+    __tablename__ = 'venues'
+
+    id = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    old_id = db.Column(db.Integer)
+    name = db.Column(db.String(255))
+    address = db.Column(db.String(255))
+    directions = db.Column(db.String(255))
+
+    default = db.Column(db.Boolean)
+
+    def serialize(self):
+        return {
+            'id': str(self.id),
+            'old_id': self.old_id,
+            'name': str(self.name),
+            'address': self.address,
+            'directions': self.directions,
+            'default': self.default,
+        }
