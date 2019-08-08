e001c50a07355f5e4f4cbb6c2e0f7683e21964b7
kenlt-uk <kenlt.uk@gmail.com>
2019-08-09 00:22:17 +0100

    Activate venv for celery

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 set +x
 
+if [ -z "$VIRTUAL_ENV" ] && [ -d venv ]; then
+  echo 'activate venv for celery'
+  source ./venv/bin/activate
+fi
+
 celery -A run_celery.celery worker --loglevel=INFO --concurrency=4
