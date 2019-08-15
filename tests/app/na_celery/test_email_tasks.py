a797f4862ccfdb84ff87f0f64a6abdc405823215
kenlt-uk <kenlt.uk@gmail.com>
2019-08-15 09:47:15 +0100

    Update email task test for members

--- tests/app/na_celery/test_email_tasks.py
+++ tests/app/na_celery/test_email_tasks.py
@@ -3,11 +3,11 @@ from app.na_celery.email_tasks import send_emails
 
 class WhenProcessingSendEmailsTask:
 
-    def it_calls_send_email_to_task(self, mocker, db, db_session, sample_admin_user, sample_email):
-        mock_send_email = mocker.patch('app.na_celery.email_tasks.send_email')
+    def it_calls_send_email_to_task(self, mocker, db, db_session, sample_email, sample_member):
+        mock_send_email = mocker.patch('app.na_celery.email_tasks.send_email', return_value=200)
         send_emails(sample_email.id)
 
-        assert mock_send_email.call_args[0][0] == 'admin@example.com'
+        assert mock_send_email.call_args[0][0] == sample_member.email
         assert mock_send_email.call_args[0][1] == 'workshop: test title'
 
     def it_sends_an_email_to_members_up_to_email_limit(self):
