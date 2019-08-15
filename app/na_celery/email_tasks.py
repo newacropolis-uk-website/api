ac1655f9b49a8a1ba34fce22a65e5ec2f350d7b1
kenlt-uk <kenlt.uk@gmail.com>
2019-08-15 09:46:42 +0100

    Update email task and dao for status_code

--- app/na_celery/email_tasks.py
+++ app/na_celery/email_tasks.py
@@ -3,25 +3,26 @@ from flask import current_app
 
 from app import celery
 from app.comms.email import send_email, get_email_html
-from app.dao.emails_dao import dao_get_email_by_id
+from app.dao.emails_dao import dao_get_email_by_id, dao_add_member_sent_to_email
+from app.dao.members_dao import dao_get_members_not_sent_to
 from app.dao.users_dao import dao_get_admin_users
 from app.models import EVENT
 
 
 @celery.task()
 def send_emails(email_id):
-    current_app.logger.info('Task send_emails received %s', email_id)
+    members_not_sent_to = dao_get_members_not_sent_to(email_id)
 
-    # use admin emails for now as members until member emails imported
-    emails_to = [admin.email for admin in dao_get_admin_users()]
+    current_app.logger.info('Task send_emails received %s, sending %d emails', email_id, len(members_not_sent_to))
 
-    for email_to in emails_to:
-        email = dao_get_email_by_id(email_id)
+    email = dao_get_email_by_id(email_id)
 
+    for member_id, email_to in members_not_sent_to:
         subject = email.get_subject()
         message = None
         if email.email_type == EVENT:
             message = get_email_html(
                 email.email_type, event_id=email.event_id, details=email.details, extra_txt=email.extra_txt)
 
-        send_email(email_to, subject, message)
+        email_status_code = send_email(email_to, subject, message)
+        dao_add_member_sent_to_email(email_id, member_id, status_code=email_status_code)
