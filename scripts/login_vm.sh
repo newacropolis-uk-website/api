e23f416e67b121673d9254cd3bd953adeba264e0
Ken Tsang <kenlt.uk@gmail.com>
2019-08-24 00:37:01 +0100

    wip

--- scripts/login_vm.sh
+++ scripts/login_vm.sh
@@ -1,4 +1,4 @@
-[[ -z "${TRAVIS_TAG}" ]] && (echo $TRAVIS_KEY | base64 --decode > travis_rsa) || echo 'no'
+[[ -z "${TRAVIS_TAG}" ]] && (echo $TRAVIS_KEY | base64 --decode > travis_rsa) && echo "using live key" || echo 'no'
 eval "$(ssh-agent -s)"
 chmod 600 travis_rsa
 ssh-add travis_rsa
